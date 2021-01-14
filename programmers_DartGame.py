d483a717e65f61aa4cc4993497100e894e4987aa
parksh-db <orpsh1941@gmail.com>
2021-01-14 18:36:00 +0900

    solution(by create score array)

--- programmers_DartGame.py
+++ programmers_DartGame.py
@@ -1,45 +1,22 @@
-import re
-
-
-def strtolst(str):
-    lstdart = list(str)
-    stack = []
-    for i in range(len(lstdart) - 1):
-        if lstdart[i] == '1':
-            if lstdart[i + 1] == '0':
-                stack.append('10')
-                del lstdart[i + 1]
-            else:
-                stack.append('1')
-        else:
-            stack.append(lstdart[i])
-
-    return stack
-
-
 def solution(dartResult):
-    stack = strtolst(dartResult)
-    #stack = ['1', 'D', '2', 'S', '#', '10', 'S']
-
-    #result = 0 #Score
-
-    # for i in range(len(stack)):
-    #     if stack[i] == 'S' or stack[i] == 'D' or stack[i] == 'T': #보너스일 경우
-    #         if stack[i + 1] is not None: #마지막 문자가 아닐 경우
-    #             if stack[i+1] == '#':
-    #                 result =
-    #             elif stack[i+1] == '*':
-    #
-    #             else:
-    #                 print(stack[i])
-    #         else: #마지막 문자일 경우
-
-
-
-
-def main():
-    dartResult = "1D2S#10S"
-    print(solution(dartResult))
-
-
-main()
+    score = []
+    n = ''
+    for i in dartResult:
+        if i.isnumeric():
+            n += i
+        elif i == 'S':
+            score.append(int(n) ** 1)
+            n = ''
+        elif i == 'D':
+            score.append(int(n) ** 2)
+            n = ''
+        elif i == 'T':
+            score.append(int(n) ** 3)
+            n = ''
+        elif i == '*':
+            if len(score) > 1:
+                score[-2] *= 2
+            score[-1] *= 2
+        elif i == '#':
+            score[-1] *= -1
+    return sum(score)
