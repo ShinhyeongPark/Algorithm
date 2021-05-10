86c96a52a5bf7d85229faf1486b59752809f713a
etlaou <57867611+ShinhyeongPark@users.noreply.github.com>
2021-05-10 22:45:59 +0900

    add comment

--- Level2/programmers_프렌즈4블록.py
+++ Level2/programmers_프렌즈4블록.py
@@ -4,31 +4,31 @@ def solution(m, n, board):
     for bo in board:
         block.append(list(bo))
 
-    while True:
+    while True: #2x2블록이 없을떄까지 반복
         answer = []
         for i in range(m-1):
             for j in range(n-1):
-                if block[i][j] == block[i+1][j] == block[i][j+1] == block[i+1][j+1]:
+                if block[i][j] == block[i+1][j] == block[i][j+1] == block[i+1][j+1]: #0이 아닌 문자로 이뤄진 2x2 블록
                     if block[i][j] != 0 and block[i+1][j] != 0 and block[i][j+1] != 0 and block[i+1][j+1] != 0:
                         answer.append([i,j])
                         answer.append([i+1,j])
                         answer.append([i,j+1])
                         answer.append([i+1,j+1])
         
-        if len(answer) == 0:
-            return result
+        if len(answer) == 0: #추가적인 블록이 없다? -> 2x2 블록이 없다.
+            return result #총 사라진 블럭 수 리턴
 
-        temp = []
+        temp = [] #중복 제거를 위한 리스트
         for i in answer:
             if i not in temp:
                 temp.append(i)
-        
         result += len(temp)
 
-        for ans in temp:
+        for ans in temp: #사라진 블럭은 0으로 변환
             x,y = ans
             block[x][y] = 0
 
+        #열을 기준으로 비교하며 계속해서 교환
         for i in range(n-1,-1,-1):
             c = []
             for j in range(m-1,-1,-1):
@@ -41,3 +41,7 @@ def solution(m, n, board):
                 else:
                     block[j][i] = 0
 
+def main():
+    print(solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
+
+main()
