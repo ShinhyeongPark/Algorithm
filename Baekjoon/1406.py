6a2416d1302c47dfa0e8220044f621e266d35fc6
etlaou <57867611+ShinhyeongPark@users.noreply.github.com>
2021-05-31 23:37:10 +0900

    Append, pop

--- Baekjoon/1406.py
+++ Baekjoon/1406.py
@@ -1,32 +1,51 @@
-string = list(input()) #abcd
-edit = [' '] + list(' '.join(string)) + [' '] #[' ', 'a', ' ', 'b', ' ', 'c', ' ', 'd', ' ']
-p = len(edit) - 1
-#insert, del 시간초가
+# string = list(input()) #abcd
+# edit = [' '] + list(' '.join(string)) + [' '] #[' ', 'a', ' ', 'b', ' ', 'c', ' ', 'd', ' ']
+# p = len(edit) - 1
 
+
+# N = int(input()) #3회
+# for n in range(N):
+#     method = list(input().split())
+
+#     if method[0] == 'L':
+#         if p == 0: #커서가 맨앞일 경우
+#             continue #무시
+#         else:
+#             p -= 2
+#     elif method[0] == 'D':
+#         if p == len(edit) - 1:
+#             continue
+#         else:
+#             p += 2
+#     elif method[0] == 'B':
+#         if p == 0:
+#             continue
+#         else:
+#             del edit[p-2:p]
+#             p -= 2
+#     elif method[0] == 'P':
+#         edit.insert(p, ' ')
+#         p += 1
+#         edit.insert(p, method[1])
+#         p += 1
+
+# print(''.join(edit).replace(' ',''))
+
+s1 = list(input()) #abcd
+s2 = []
 N = int(input()) #3회
+#Insert, Del을 Stack 두개를 사용해 pop, append로 변경
+#O(1)
 for n in range(N):
     method = list(input().split())
 
-    if method[0] == 'L':
-        if p == 0: #커서가 맨앞일 경우
-            continue #무시
-        else:
-            p -= 2
-    elif method[0] == 'D':
-        if p == len(edit) - 1:
-            continue
-        else:
-            p += 2
-    elif method[0] == 'B':
-        if p == 0:
-            continue
-        else:
-            del edit[p-2:p]
-            p -= 2
+    if method[0] == 'L' and s1 != []:
+        s2.append(s1.pop())
+    elif method[0] == 'D' and s2 != []:
+        s1.append(s2.pop())
+    elif method[0] == 'B' and s1 != []:
+        s1.pop()
     elif method[0] == 'P':
-        edit.insert(p, ' ')
-        p += 1
-        edit.insert(p, method[1])
-        p += 1
+        s1.append(method[1])
 
-print(''.join(edit).replace(' ',''))
+print("".join(s1 + list(reversed(s2))))
