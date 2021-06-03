cf515f4fed4af18035945c8c8aa73d883c75dec3
etlaou <57867611+ShinhyeongPark@users.noreply.github.com>
2021-06-03 10:24:22 +0900

    공항(Not Union-find)

---
+++ Baekjoon/10775.py
@@ -0,0 +1,36 @@
+import sys
+
+G = int(input()) #게이트수
+P = int(input()) #비행기수
+
+airlines = []
+
+#게이트가 꽉찰 경우 break
+#게이트가 아직 비어있으나 자기가 들어갈 자리가 없을 경우
+
+#GATE [0,0,0,0]
+#P = 6
+port = []
+for p in range(P):
+    port.append(int(input()))
+
+for x in port:
+    flag = 0
+    if len(airlines) == G:
+        break
+    else:
+        if x not in airlines: #x번호가 아직 도킹이 되어 있지 않을 경우
+            airlines.append(x) #추가
+            flag = 1
+        else: #x번호가 이미 도킹되어 있을 경우
+            for y in range(1,x): #자기번호보다 낮은 번호 검색
+                if y not in airlines: #y가 아직 도킹 되어 있지 않을 경우
+                    airlines.append(y)
+                    flag = 1
+                    break
+                if flag == 0:
+                    break
+    if flag == 0:
+        break
+
+print(len(airlines))
