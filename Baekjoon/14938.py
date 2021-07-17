6827cc1b5e21fdca335526afa72094c884eca7d8
etlaou <57867611+ShinhyeongPark@users.noreply.github.com>
2021-07-17 15:38:26 +0900

    다익스트라

---
+++ Baekjoon/14938.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def dfs(start, visit, item, dis):
+    visit[start] = True
+    item += T[start]
+
+    if roads[start]:
+        for next in roads[start]:
+            if next[1] + dis < M and visit[next[0]] == False:
+                item += dfs(next[0], visit, 0, next[1] + dis)
+    else:
+        return item
+
+    return item
+
+
+N, M, R = map(int, sys.stdin.readline().split())
+
+T = [0] + list(map(int, sys.stdin.readline().split()))
+
+roads = [[] for _ in range(N+1)]
+
+for _ in range(R):
+    start, end, cost = map(int, sys.stdin.readline().split())
+
+    roads[start].append([end, cost])
+    roads[end].append([start, cost])
+
+itemCount = 0
+for startNode in range(1, N+1):
+    visited = [False] * (N+1)
+    itemCount = max(itemCount, dfs(startNode, visited, 0, 0))
+
+print(itemCount)
