893b7b235bb3a630c2098fb8aa3a749fef4e3311
etlaou <57867611+ShinhyeongPark@users.noreply.github.com>
2021-08-02 13:43:54 +0900

    Update 10775.py

--- Baekjoon/10775.py
+++ Baekjoon/10775.py
@@ -34,3 +34,34 @@ for x in port:
         break
 
 print(len(airlines))
+
+# import sys
+# g = int(sys.stdin.readline())
+# p = int(sys.stdin.readline())
+# plane = []
+# for _ in range(p):
+#     plane.append(int(sys.stdin.readline()))
+
+# def parent_find(x):
+#     if x == parent[x]:
+#         return x
+#     p = parent_find(parent[x])
+#     parent[x] = p
+#     return parent[x]
+
+# def union(x, y):
+#     x = parent_find(x)
+#     y = parent_find(y)
+#     parent[x] = y
+
+# # parent[0] = 0 까지 만들어준다. parent[x] = 0까지 만들어지는 게 핵심    
+# parent = {i:i for i in range(g+1)}
+# cnt = 0
+# for i in plane:
+#     x = parent_find(i)
+#     if x == 0:
+#         break
+#     union(x, x-1)
+#     cnt += 1
+# print(cnt)
+    
