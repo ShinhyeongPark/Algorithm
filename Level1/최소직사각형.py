0f1443c233d426d9a0f662ac6d19ecdd8c05fe56
etlaou <57867611+ShinhyeongPark@users.noreply.github.com>
2021-10-19 19:58:40 +0900

    구현 comment

--- Level1/최소직사각형.py
+++ Level1/최소직사각형.py
@@ -1,38 +1,37 @@
 def solution(sizes):
-    maxAns = 0
+    maxAns = 0  # 기준 열의 최대값
+    minAns = 0  # 기준이 아닌 열의 최대값
 
-    rowA = 0
-    rowB = 0
+    rowA = 0  # First Row Max Value
+    rowB = 0  # Second Row Max Value
 
-    default = 0  # 기준 열을 첫번째 열로
-    # A열과 B열의 최대값을 구했다.
+    default = 0  # Standard Row (Start is First Row)
+
+    # Calculate Max in First Row and Second Row
     for size in sizes:
         if size[0] > rowA:
             rowA = size[0]
         if size[1] > rowB:
             rowB = size[1]
 
+    # if max in second row is bigger thab first row
     if rowB > rowA:
-        default = 1  # B열의 최대값이 가장 클 경우, 기준 열을 두번쩨 열로
-        maxAns = rowB
+        default = 1  # standard row changes to second row
+        maxAns = rowB  # max Values changes to max value of second row
     else:
         maxAns = rowA
 
-    # minAns = sizes[0][ans(default-1)]
-
-    for i in range(len(sizes)):  # 행만큼 반복
+    # loop for length of sizes
+    for i in range(len(sizes)):
         if sizes[i][default] == maxAns:
             continue
 
         if sizes[i][default] < sizes[i][abs(default-1)]:
-            # print(i)
             sizes[i][default], sizes[i][abs(
                 default-1)] = sizes[i][abs(default-1)], sizes[i][default]
 
-    # print(sizes)
-    minAns = 0
     for size in sizes:
         if size[abs(default-1)] > minAns:
             minAns = size[abs(default-1)]
-    # print(minAns)
+
     return maxAns * minAns
